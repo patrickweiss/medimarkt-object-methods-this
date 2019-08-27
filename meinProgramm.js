4187ce3184ea805b67bfcab56c16be3ba3510c7b
Patrick Weiss <patrick.weiss@officeone.team>
2019-08-27 11:29:02 +0200

    function declaration expression this

---
+++ meinProgramm.js
@@ -0,0 +1,61 @@
+// Example of a VERY simple Monopoly game simulation
+
+let squares = [100, -10, 0, 0, -40, -10, -10, 5, 0, -10, -50, -10, 0, 0, -50, -10]
+
+// --- Initialization with methods ---
+
+
+function bewegDich() {
+    let dice = 1 + Math.floor(6 * Math.random());
+    this.position = (this.position + dice) % squares.length;
+    this.cash += squares[this.position];
+    if (this.cash < 0) {
+        console.log(`Game over for ${this.name}.`);
+    }
+}
+
+const zeigeErgebnis = function () {
+    console.log(`${this.name} is at position ${this.position} and has ${this.cash}€`);
+}
+
+let player1 = {
+    name: 'Joaquim',
+    color: 'red',
+    position: 0,
+    cash: 1000,
+    move: bewegDich,
+    displayInfo:zeigeErgebnis
+}
+
+let player2 = {
+    name: 'Maxence',
+    color: 'blue',
+    position: 0,
+    cash: 1000,
+    move: bewegDich,
+    displayInfo:zeigeErgebnis
+}
+
+let player3 = {
+    name: 'Mostafa',
+    color: 'black',
+    position: 0,
+    cash: 1000,
+    move: bewegDich,
+    displayInfo:zeigeErgebnis
+}
+
+// --- Turn 1  ---
+player1.move();
+player2.move();
+player3.move();
+
+// --- Turn 2  ---
+player1.move();
+player2.move();
+player3.move();
+
+// --- Display info  ---
+player1.displayInfo();
+player2.displayInfo();
+player3.displayInfo();
